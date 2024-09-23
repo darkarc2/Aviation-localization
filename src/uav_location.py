b12b936f01cee302dea62dd2559cc1d29194091e
darkarc <2350230863@qq.com>
2024-09-23 14:51:03 +0800

    临时缝合

--- src/uav_location.py
+++ src/uav_location.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 import time
+from Image_Viewer import ImageViewer
+
 
 class Frame:
     _id_counter = 0  # 类变量，用于计数
@@ -25,8 +27,6 @@ class Frame:
         Frame._id_counter += 1
         if self.object_id == 0:
             self.uv_pose = np.array([0, 0])
-        # if self.object_id == 1:
-        #     self.uv_pose = np.array([-161.0696106,-865.79919434])
 
     # 生成图像金字塔,levels表示金字塔层数
     def generate_pyramid(self, image, levels=3):
@@ -131,16 +131,43 @@ class GlobalMap:
         self.stitched_active_frames = stitched_active_frames  # 活动图像拼接结果
         self.all_frames=all_frames #所有帧对象
 
-
-    def draw_lines_between_centers(self, image):
-        # 获取每个帧的中心点
-        centers = [frame.uv_pose for frame in self.active_frames.frames if frame.uv_pose is not None]
-        # 将中心点转换为整数坐标
-        centers = [(int(center[0]), int(center[1])) for center in centers]
-        # 画连线
-        for i in range(1, len(centers)):
-            cv2.line(image, centers[i-1], centers[i], (0, 0, 255), 20)
-        return image
+    def compute_translation(self):
+        # 拼接活动图像的逻辑
+        if not self.active_frames or len(self.active_frames.frames) == 0:
+            raise ValueError("No active images to stitch.")
+        if len(self.active_frames.frames)==1:
+            return self.active_frames.frames[0].pyramid_images[2]
+        # 获取所有活动帧的图像
+        images = [frame.pyramid_images[2] for frame in self.active_frames.frames]
+        keypoints_list = [frame.keypoints for frame in self.active_frames.frames]
+    
+        for i in range(1, len(images)):
+            # 检查当前帧的uv_pose是否为None
+            if self.active_frames.frames[i].uv_pose is not None:
+                deta_pose = self.active_frames.frames[i].uv_pose-self.active_frames.frames[i-1].uv_pose
+                # new_image = self.translation_and_warp(images[i-1], images[i], deta_pose)
+                # print("直接拼接，不计算")
+            else:
+                # 获取当前图像和基准图像的特征点
+                keypoints1 = [kp for kp in keypoints_list[i-1] if kp.pyramid_layer == 2]
+                keypoints2 = [kp for kp in keypoints_list[i] if kp.pyramid_layer == 2]
+                
+                # 使用BFMatcher进行特征点匹配
+                bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+                descriptors1 = np.array([kp.descriptor for kp in keypoints1])
+                descriptors2 = np.array([kp.descriptor for kp in keypoints2])
+    
+                matches = bf.match(descriptors1, descriptors2)
+                matches = sorted(matches, key=lambda x: x.distance)
+                
+                # 获取匹配点
+                points1 = [keypoints1[m.queryIdx].pt for m in matches]
+                points2 = [keypoints2[m.trainIdx].pt for m in matches]
+                
+                # 计算平移并进行图像拼接
+                deta_pose = self.compute_translation_and_warp(images[i-1], images[i], points1, points2)
+                self.active_frames.frames[i].uv_pose = deta_pose + self.active_frames.frames[i-1].uv_pose
+                print(f"帧{self.active_frames.frames[i].object_id}的uv_pose为{self.active_frames.frames[i].uv_pose}")
 
     # 计算特征点匹配的平移向量
     def compute_translation_and_warp(self, img_1, img_2, points1, points2):
@@ -177,135 +204,11 @@ class GlobalMap:
         
         # 重新计算平移向量
         translation_vector = -1 * np.mean(filtered_points2 - filtered_points1, axis=0)
-        
-        # 进行图像拼接
-        result=self.translation_and_warp(img_1, img_2, translation_vector)
 
         # 保存平移向量到pose变量
         deta_pose = translation_vector
         
-        return result, deta_pose
-    
-    # 基于detapose图像平移和拼接
-    def translation_and_warp(self, img_1, img_2,deta_pose):
-        # 计算平移矩阵
-        M = np.array([[1, 0, deta_pose[0]], [0, 1, deta_pose[1]]], dtype=np.float32)
-        
-        # 获取图像尺寸
-        h1, w1 = img_1.shape[:2]
-        h2, w2 = img_2.shape[:2]
-        
-        # 计算拼接后的图像尺寸
-        x_min = min(0, deta_pose[0])
-        y_min = min(0, deta_pose[1])
-        x_max = max(w1, w2 + deta_pose[0])
-        y_max = max(h1, h2 + deta_pose[1])
-        
-        # 创建一个足够大的画布
-        canvas_width = int(x_max - x_min)
-        canvas_height = int(y_max - y_min)
-        result = np.zeros((canvas_height, canvas_width, 3), dtype=img_1.dtype)
-        
-        # 将第一张图片放到画布上
-        result[-int(y_min):h1-int(y_min), -int(x_min):w1-int(x_min)] = img_1
-        
-        # 将第二张图片平移后放到画布上
-        result[int(deta_pose[1])-int(y_min):int(deta_pose[1])-int(y_min)+h2, int(deta_pose[0])-int(x_min):int(deta_pose[0])-int(x_min)+w2] = img_2
-        
-        return result
-    
-    def stitch_active_frames(self):
-        # 拼接活动图像的逻辑
-        if not self.active_frames or len(self.active_frames.frames) == 0:
-            raise ValueError("No active images to stitch.")
-        if len(self.active_frames.frames)==1:
-            return self.active_frames.frames[0].pyramid_images[2]
-        # 获取所有活动帧的图像
-        images = [frame.pyramid_images[2] for frame in self.active_frames.frames]
-        keypoints_list = [frame.keypoints for frame in self.active_frames.frames]
-    
-        # print(f"正在拼接{len(images)}张图像:开始帧{self.active_frames.frames[0].object_id}，结束帧{self.active_frames.frames[-1].object_id}")
-        poses = []
-        global_map=None
-        for i in range(1, len(images)):
-            # 检查当前帧的uv_pose是否为None
-            if self.active_frames.frames[i].uv_pose is not None:
-                deta_pose = self.active_frames.frames[i].uv_pose-self.active_frames.frames[i-1].uv_pose
-                new_image = self.translation_and_warp(images[i-1], images[i], deta_pose)
-                # print("直接拼接，不计算")
-            else:
-                # 获取当前图像和基准图像的特征点
-                keypoints1 = [kp for kp in keypoints_list[i-1] if kp.pyramid_layer == 2]
-                keypoints2 = [kp for kp in keypoints_list[i] if kp.pyramid_layer == 2]
-                
-                # 使用BFMatcher进行特征点匹配
-                bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-                descriptors1 = np.array([kp.descriptor for kp in keypoints1])
-                descriptors2 = np.array([kp.descriptor for kp in keypoints2])
-    
-                matches = bf.match(descriptors1, descriptors2)
-                matches = sorted(matches, key=lambda x: x.distance)
-                
-                # 获取匹配点
-                points1 = [keypoints1[m.queryIdx].pt for m in matches]
-                points2 = [keypoints2[m.trainIdx].pt for m in matches]
-                
-                # 计算平移并进行图像拼接
-                new_image, deta_pose = self.compute_translation_and_warp(images[i-1], images[i], points1, points2)
-                self.active_frames.frames[i].uv_pose = deta_pose + self.active_frames.frames[i-1].uv_pose
-            if global_map is None:
-                global_map = new_image
-            else:
-                # deta_pose = self.active_frames.frames[i].uv_pose-self.active_frames.frames[i-1].uv_pose
-                global_map = self.translation_and_warp(global_map, new_image, deta_pose)
-        
-        # 更新reduced_image属性
-        self.reduced_image = global_map
-        self.poses = poses
-         # 在拼接后的图像上画连线
-        self.reduced_image = self.draw_lines_between_centers(self.reduced_image)
-
-        return self.reduced_image
-    
-
-    def dynamic_stitch_based_on_observation(self):
-
-         # 开始计时
-        start_time = time.time()
-
-        if self.observation_frame_id is None:
-            raise ValueError("Observation frame ID is not set.")
-        
-
-        before_observation_flag=True
-        activate_count=0
-        if len(self.all_frames)==0: #如果没有帧，直接返回
-            return self.stitch_active_frames()
-        for frame in self.all_frames:
-            if frame.object_id == self.observation_frame_id:
-                before_observation_flag=False # 找到观察框所在帧，后续帧需要激活
-            if before_observation_flag :
-                if frame.is_active==True:
-                    self.active_frames.remove_frame(frame)  # 移除观察框之前的活动帧
-            elif activate_count<=self.active_frames.activate_len: #控制激活的帧数
-                if frame.is_active==False:
-                    self.active_frames.reactivate_frame(frame) # 重新激活观察框之后的帧
-                    self.active_frames.add_frame(frame) # 添加到活动帧列表
-                activate_count+=1 # 计数已激活的帧数
-            else:
-                break
-
-        stitched_image = self.stitch_active_frames()  # 拼接活动帧
-
-        # 结束计时
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"dynamic_stitch_based_on_observation 耗时: {elapsed_time:.4f} 秒")
-
-        
-        return stitched_image
-
-
+        return deta_pose
 
 class UAV_LOCATION:
 
@@ -319,11 +222,9 @@ class UAV_LOCATION:
         frame = Frame(image_path)
         self.frames.append(frame)
         self.active_frames.add_frame(frame)
-        # self.map_img=self.map.stitch_active_frames()
-
         # 时刻取最新的地图，观察窗应该从最新帧id-活动地图帧数量开始
         self.map.observation_frame_id = max(0,self.frames[-1].object_id-self.active_frames.activate_len+1)
-        self.map_img=self.map.dynamic_stitch_based_on_observation()
+        self.map.compute_translation()
 
     
     def get_global_map(self):
@@ -336,54 +237,6 @@ class UAV_LOCATION:
         self.map.observation_frame_id=observation_frame_id
 
 
-def show_frames(frames):
-    for frame in frames:
-        cv2.imshow("Frame", frame.pyramid_images[-1])
-        cv2.waitKey(0)
-    return
-
-
-
-# 定义鼠标事件回调函数
-def mouse_callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        param['dragging'] = True
-        param['start_x'] = x
-        param['start_y'] = y
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if param['dragging']:
-            pass
-
-    elif event == cv2.EVENT_LBUTTONUP:
-        param['dragging'] = False
-        dx = x - param['start_x']
-        dy = y - param['start_y']
-        param['start_x'] = x
-        param['start_y'] = y
-        if abs(dy) < 50:
-            dy = 0
-        
-        # 更新观察框的位置
-        change_flag=True
-        if dy > 0:
-            param['uav'].map.observation_frame_id += 1
-            if param['uav'].map.observation_frame_id >= len(param['uav'].frames)-param['uav'].active_frames.activate_len:
-                param['uav'].map.observation_frame_id = param['uav'].map.observation_frame_id - 1
-                change_flag=False
-        elif dy < 0:
-            param['uav'].map.observation_frame_id -= 1
-            if param['uav'].map.observation_frame_id < 0:
-                param['uav'].map.observation_frame_id = 0
-                change_flag=False
-        else:
-            change_flag=False
-
-        if change_flag:
-            # 动态加载附近的活动帧
-            stitched_image = param['uav'].map.dynamic_stitch_based_on_observation()
-            # 显示更新后的地图
-            cv2.imshow("g_map", stitched_image)
-
 
 if __name__ == "__main__":
     image_dir = '/mnt/d/Dataset/UAV_VisLoc_dataset/03/drone/' 
@@ -391,36 +244,19 @@ if __name__ == "__main__":
     img_count = 0  
     uav=UAV_LOCATION()
 
-    # 设置初始的观察框位置
-    observation_frame_id = 0
-    uav.set_map_observaion_id(observation_frame_id)
+    viewer = ImageViewer()
 
-     # 初始化鼠标事件参数
-    mouse_params = {'dragging': False, 'start_x': 0, 'start_y': 0,'uav':uav}
-
-    # 创建窗口并设置鼠标事件回调函数
-    cv2.namedWindow("g_map")
-    cv2.setMouseCallback("g_map", mouse_callback, mouse_params)
     
     for filename in sorted(os.listdir(image_dir)):
         if filename.endswith(".JPG"):
             img_count+=1
             if img_count>20:
                 break
-
-
             path=os.path.join(image_dir, filename)
             uav.read_frame(path)
-            global_map=uav.get_global_map()
-            # cv2.imshow("g_map",global_map)
-            # cv2.waitKey(0)
+            viewer.add_image(path, uav.frames[-1].uv_pose*9)
 
-
-            # 显示初始的全局地图
-            # stitched_image = uav.map.dynamic_stitch_based_on_observation()
-            cv2.imshow("g_map", global_map)
-            cv2.waitKey(1)
-    cv2.waitKey(0)
+    viewer.run()
     cv2.destroyAllWindows()
 
 
